207daecb19bcbab6e2adaab9c9db166675c58a6a
Aaron Kling <webgeek1234@gmail.com>
2019-05-09 09:41:14 -0500

    Add nvphs hal
    Change-Id: Ie3f401e02faf6cc9616fb46abc9f00198c31baf3

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ TARGET_TEGRA_BT       ?= bcm
 TARGET_TEGRA_CEC      ?= nvhdmi
 TARGET_TEGRA_MEMTRACK ?= nvmemtrack
 TARGET_TEGRA_OMX      ?= nvmm
+TARGET_TEGRA_PHS      ?= nvphs
 TARGET_TEGRA_POWER    ?= aosp
 TARGET_TEGRA_WIFI     ?= bcm
 
@@ -112,6 +113,12 @@ ifeq ($(TARGET_TEGRA_OMX),nvmm)
 PRODUCT_PACKAGES += \
     media_codecs_performance.xml \
     media_profiles.xml
+endif
+
+# PHS
+ifeq ($(TARGET_TEGRA_PHS),nvphs)
+PRODUCT_PACKAGES += \
+    nvphsd.conf
 endif
 
 # Thermal
