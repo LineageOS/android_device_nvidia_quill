LineageOS/android_device_nvidia_foster

Include vendor files
This will check for either the shield unified vendor or a device
specific one created by the extract scripts.

Change-Id: Ibf54e898b1066b07c2bb148ef49dd516a00153d4

device.mk
@@ -24,6 +24,8 @@ TARGET_SCREEN_WIDTH      := 1080
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
+$(call inherit-product, vendor/nvidia/foster/foster-vendor.mk)
+
 # Init related
 PRODUCT_PACKAGES += \
     fstab.darcy \

LineageOS/android_device_nvidia_quill

Change-Id: I839711e4a8c674ad5fcf42f9bdd6998619fd002d

device.mk
@@ -38,6 +38,8 @@ TARGET_SCREEN_WIDTH      := 1080
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
+$(call inherit-product, vendor/nvidia/quill/quill-vendor.mk)
+
 # Init related
 PRODUCT_PACKAGES += \
     fstab.lightning \