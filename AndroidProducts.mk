LineageOS/android_device_nvidia_foster

Refactor twrp target
TeamWin is now targeting aosp to build twrp. Make a separate target
to allow differentiating twrp and normal aosp. Include the lineage
makefile for the unified build flags. The other flags wont apply
when only building recovery

Change-Id: I8d067b50d02d6e20e8865bdc4dcc414dd73b4c4b

AndroidProducts.mk
@@ -17,4 +17,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_foster.mk \
     $(LOCAL_DIR)/lineage_foster.mk \
-    $(LOCAL_DIR)/omni_foster.mk
+    $(LOCAL_DIR)/twrp_foster.mk

LineageOS/android_device_nvidia_quill

AndroidProducts.mk
@@ -17,4 +17,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_quill.mk \
     $(LOCAL_DIR)/lineage_quill.mk \
-    $(LOCAL_DIR)/omni_quill.mk
+    $(LOCAL_DIR)/twrp_quill.mk