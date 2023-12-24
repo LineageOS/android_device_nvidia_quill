aa4d05fc0c698cf0d34a4619697c5d04d7ab94a9
Aaron Kling <webgeek1234@gmail.com>
2023-12-24 14:31:24 -0600

    Support android common kernel
    The lowest supported version is 6.12.
    
    Change-Id: I4dbb7434853330abdfe8ebc21b52e5d67737ffca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,7 @@ TARGET_VENDOR_PROP += device/nvidia/quill/bluetooth.prop
 BOARD_MKBOOTIMG_ARGS := --header_version 1
 
 # Kernel
+ifeq ($(TARGET_KERNEL_VERSION),4.9)
 TARGET_KERNEL_CLANG_COMPILE    := false
 KERNEL_TOOLCHAIN               := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-9.3/bin
 KERNEL_TOOLCHAIN_PREFIX        := aarch64-buildroot-linux-gnu-
@@ -90,6 +91,15 @@ MODDIR := $(dir $(TARGET_PREBUILT_KERNEL))
 BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(MODDIR)/*.ko)
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(MODDIR)/,$(BOOT_KERNEL_MODULES))
 BOARD_RECOVERY_KERNEL_MODULES := $(addprefix $(MODDIR)/,$(RECOVERY_KERNEL_MODULES))
+endif
+else
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+TARGET_KERNEL_PLATFORM_TARGET := tegra
+TARGET_KERNEL_SOURCE          := vendor/nvidia/$(TARGET_KERNEL_PLATFORM_TARGET)
+BOARD_KERNEL_IMAGE_NAME       := Image
+endif
+BOARD_KERNEL_CMDLINE          := firmware_class.path=/vendor/firmware cpufreq.default_governor=performance nouveau.atomic=1 cma=512MB
+include device/nvidia/quill/modules-ack.mk
 endif
 
 # Recovery
