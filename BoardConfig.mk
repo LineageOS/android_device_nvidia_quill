c93409155be9e15daa630b75f6e585a9eec68c13
Aaron Kling <webgeek1234@gmail.com>
2024-03-31 15:19:16 -0500

    Build exfat support
    Change-Id: I1231161d7f467626a6c5bcb0ce9cb455b91b03bd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,11 +82,12 @@ KERNEL_TOOLCHAIN_PREFIX        := aarch64-buildroot-linux-gnu-
 TARGET_KERNEL_SOURCE           := kernel/nvidia/kernel-$(TARGET_TEGRA_KERNEL)
 TARGET_KERNEL_CONFIG           := tegra_android_defconfig
 BOARD_KERNEL_IMAGE_NAME        := Image.gz
-TARGET_KERNEL_ADDITIONAL_FLAGS := "NV_BUILD_KERNEL_OPTIONS=$(TARGET_TEGRA_KERNEL)"
+TARGET_KERNEL_ADDITIONAL_FLAGS := NV_BUILD_KERNEL_OPTIONS=$(TARGET_TEGRA_KERNEL) CONFIG_EXFAT_FS=m
 BOARD_KERNEL_CMDLINE           := "androidboot.boot_devices=3460000.sdhci"
 
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/nvidia
 TARGET_KERNEL_EXT_MODULES := \
+    exfat:kbuild \
     nvgpu/drivers/gpu/nvgpu:kbuild
 include device/nvidia/quill/modules.mk
 
