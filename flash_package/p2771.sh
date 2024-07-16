fe549545a495cdddcf8d1f4b8f2b953ebcde76cf
Aaron Kling <webgeek1234@gmail.com>
2024-07-15 22:59:11 -0500

    flash_package: Support flashing with locked bl
    Change-Id: Icd323f4339233d60a6ee30a7fb2dcf3f61979449

--- flash_package/p2771.sh
+++ flash_package/p2771.sh
@@ -20,6 +20,58 @@ TARGET_TEGRA_VERSION=t186;
 TARGET_MODULE_ID=3310;
 TARGET_CARRIER_ID=2597;
 
+FLASH_XML="flash_android_t186.xml";
+LOCK=0;
+
+# Bit 15: Tegra watchdog timer
+# Bit 16: Denver watchdog timer
+# Bit 19: Enable debug port
+# Bit 24: Uphy lane 0 enable xusb
+ODMDATA=$((1<<15 | 1<<16 | 1<<19 | 1<<24))
+
+VALID_ARGS=$(getopt -o hl:: --long help,lock:: -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -l | --lock)
+      if [ "$2" == "0" ]; then
+        LOCK=0;
+      elif [ -z "$2" -o "$2" == "1" ]; then
+        LOCK=1;
+      else
+        echo "Invalid value for lock";
+        exit 1;
+      fi;
+      shift 2
+      ;;
+    --) shift;
+      break
+      ;;
+    ? | h | --help)
+      echo "Usage: $(basename $0) [-n|--nvme]"
+      exit 1
+      ;;
+  esac
+done
+
+if [ ${LOCK} -eq 1 ]; then
+  if [ ! -f "avb_pkmd.bin" -o ! -f "vbmeta.img" ]; then
+    echo "avb_pkmd.bin and vbmeta.img are required to boot in locked state.";
+    exit 1;
+  fi;
+
+  sed '0,/vbmeta_skip.img/s//vbmeta.img/' ${FLASH_XML} > flash.xml;
+  sed -i '/name="avb_custom_key"/a \ \ \ \ \ \ \ \ \ \ \ \ <filename> avb_pkmd.bin </filename>' flash.xml;
+  FLASH_XML="flash.xml";
+
+  # Bit 13: Bootloader Lock State
+  ODMDATA=$((${ODMDATA} | 1<<13));
+fi;
+
 source $(pwd)/scripts/helpers.sh;
 
 declare -a FLASH_CMD_EEPROM=(
@@ -61,7 +113,7 @@ declare -a FLASH_CMD_FLASH=(
   ${FLASH_CMD_EEPROM[@]}
   --bl nvtboot_recovery_cpu.bin
   --sdram_config P3310_A00_8GB_lpddr4_A02_l4t.cfg
-  --odmdata 0x1098000
+  --odmdata $(printf "0x%x" ${ODMDATA})
   --misc_config tegra186-mb1-bct-misc-si-l4t.cfg
   --pinmux_config tegra186-mb1-bct-pinmux-quill-p3310-1000-c03.cfg
   --pmic_config tegra186-mb1-bct-pmic-quill-p3310-1000-${PMIC_CFG_VER}.cfg
@@ -79,7 +131,10 @@ cp tegra186-a02-bpmp-quill-p3310-1000-${BPF_DTB_VER}-00-te770d-ucm2.dtb tegra186
 tegraflash.py \
   "${FLASH_CMD_FLASH[@]}" \
   --instance ${INTERFACE} \
-  --cfg flash_android_t186.xml \
+  --cfg ${FLASH_XML} \
   --cmd "flash; reboot"
 
 rm p2771-0000-devkit.bin tegra186-bpmp.dtb emmc_bootblob_ver.txt;
+if [ ${LOCK} -eq 1 ]; then
+  rm ${FLASH_XML};
+fi;
