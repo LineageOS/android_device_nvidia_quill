df8040c2de2a85d42b12df96b42b8578b0873567
Aaron Kling <webgeek1234@gmail.com>
2024-12-18 22:59:11 -0600

    vendor: Move public libraries to tegra-common
    Change-Id: I03370a071b40e12c72c96b17510abea193351f07

--- vendor/quill-vendor.mk
+++ vendor/quill-vendor.mk
@@ -21,9 +21,6 @@ include device/nvidia/quill/vendor/$(TARGET_TEGRA_FIRMWARE_BRANCH)/bcm/bcm.mk
 include device/nvidia/tegra-common/vendor/$(TARGET_TEGRA_FIRMWARE_BRANCH)/bcm/bcm4354.mk
 else ifneq ("$(wildcard device/nvidia/quill/vendor/$(TARGET_TEGRA_L4T_BRANCH)/bcm_firmware/bcm.mk)","")
 include device/nvidia/quill/vendor/$(TARGET_TEGRA_L4T_BRANCH)/bcm_firmware/bcm.mk
-
-# This needs a better check. Assumption is that when firmware is from r32, nvgpu is in use.
-PRODUCT_PACKAGES += public.libraries
 endif
 
 ifneq ("$(wildcard device/nvidia/tegra-common/vendor/$(TARGET_TEGRA_FIRMWARE_BRANCH)/realtek/rtl8822ce.mk)","")
