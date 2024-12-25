c8fe3986128d3bfc2b776b52faffedb5f1042d94
Aaron Kling <webgeek1234@gmail.com>
2024-12-25 14:06:55 -0600

    Remove wireguard oot module
    Change-Id: I6b43c6e255c7217e09d189f96090a41af250a861

--- device.mk
+++ device.mk
@@ -34,7 +34,6 @@ TARGET_TEGRA_LIGHT    ?= lineage
 TARGET_TEGRA_THERMAL  ?= lineage
 TARGET_TEGRA_WIDEVINE ?= rel-shield-r
 TARGET_TEGRA_WIFI     ?= bcm
-TARGET_TEGRA_WIREGUARD ?= compat
 
 include device/nvidia/t186-common/t186.mk
 
