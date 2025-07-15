eb356ccc3534d6c285187fc8dbd99b39f6263262
Aaron Kling <webgeek1234@gmail.com>
2025-07-15 15:34:58 -0500

    Convert TARGET_TEGRA_KERNEL to TARGET_KERNEL_VERSION
    There was no common kernel version flag when this was first added, now
    there is. So change all references to use the common name.
    
    Change-Id: I206a77e8f5cb6a20da9b47b1a6592ec714f443d0

--- build/tasks/firmware.mk
+++ build/tasks/firmware.mk
@@ -34,7 +34,7 @@ KERNEL_OUT ?= $(PRODUCT_OUT)/obj/KERNEL_OBJ
 
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 DTB_PATH := $(dir $(TARGET_PREBUILT_KERNEL))
-else ifneq ($(filter 4.9, $(TARGET_TEGRA_KERNEL)),)
+else ifneq ($(filter 4.9, $(TARGET_KERNEL_VERSION)),)
 DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts)
 else ifneq ($(findstring dtstree,$(TARGET_KERNEL_ADDITIONAL_FLAGS)),)
 DTB_PATH := $(abspath $(KERNEL_OUT)/../lineage-oot/device-tree/platform/generic-dts/t18x/lineage)
--- device.mk
+++ device.mk
@@ -24,10 +24,10 @@ TARGET_TEGRA_VARIANT    ?= common
 
 TARGET_TEGRA_MODELS := $(shell awk -F, '/tegra_init::devices/{ f = 1; next } /};/{ f = 0 } f{ gsub(/"/, "", $$3); gsub(/ /, "", $$3); print $$3 }' device/nvidia/$(TARGET_REFERENCE_DEVICE)/init/init_$(TARGET_REFERENCE_DEVICE).cpp |sort |uniq)
 
+TARGET_KERNEL_VERSION ?= 4.9
 TARGET_TEGRA_BOOTCTRL ?= smd
 TARGET_TEGRA_BT       ?= bcm btlinux
 TARGET_TEGRA_CAMERA   ?= rel-shield-r
-TARGET_TEGRA_KERNEL   ?= 4.9
 TARGET_TEGRA_HEALTH   ?= nobattery
 TARGET_TEGRA_TOS      ?= software
 TARGET_TEGRA_LIGHT    ?= lineage
