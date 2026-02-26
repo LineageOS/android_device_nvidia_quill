696b35cb5e8e8960ce1fa7fb74b4c3000253070e
Aaron Kling <webgeek1234@gmail.com>
2026-02-26 12:15:07 -0600

    Update fingerprints to match stock t210 9.2.4
    Change-Id: Ibb5f4e2b054cc5d94f5b498d42858b7ae1f26299

--- init/init_quill.cpp
+++ init/init_quill.cpp
@@ -70,7 +70,7 @@ void vendor_load_properties()
 		{ "quill",    "lighting", "quill",   "Jetson TX2 4GB", 3489,    1, 28, 320 },
 		{ "lanai",    "lanai",    "lanai",   "Jetson TX2 NX",  3636,    1, 28, 320 },
 	};
-	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "7825230_4040.2147" };
+	tegra_init::build_version tav = { "11", "RQ1A.210105.003", "7825230_4387.0822" };
 
 	tegra_init ti(devices);
 	ti.set_properties();
